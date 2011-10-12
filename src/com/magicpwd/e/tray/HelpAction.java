ef332eae8a589428555802620348ae8447e816b2
Amon Yao <amonsoft@gmail.com>
2011-10-12 21:50:18 +0800

    帮助文件调整。

--- src/com/magicpwd/e/tray/HelpAction.java
+++ src/com/magicpwd/e/tray/HelpAction.java
@@ -27,7 +27,6 @@ import com.magicpwd._util.Logs;
  */
 public class HelpAction extends ATrayAction
 {
-
     public HelpAction()
     {
     }
@@ -40,17 +39,11 @@ public class HelpAction extends ATrayAction
             Lang.showMesg(trayPtn.getMpwdPtn(), LangRes.P30F7A0F, "");
         }
 
-        java.io.File help = new java.io.File("help", "index.html");
-        if (!help.exists())
-        {
-            Lang.showMesg(trayPtn.getMpwdPtn(), LangRes.P30F7A10, "");
-            return;
-        }
         try
         {
-            java.awt.Desktop.getDesktop().browse(help.toURI());
+            java.awt.Desktop.getDesktop().browse(new java.net.URI("http://mpwd.amon.me/help/"));
         }
-        catch (java.io.IOException exp)
+        catch (Exception exp)
         {
             Logs.exception(exp);
         }
